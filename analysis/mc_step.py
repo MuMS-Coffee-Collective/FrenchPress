644428eb1b7457ed258f7ed7b1ec88b0d094cd46
Christoph Klein <christoph.t.klein@me.com>
2014-02-18 15:38:37 -0600

    fancyness

--- analysis/mc_step.py
+++ analysis/mc_step.py
@@ -45,19 +45,37 @@ weight = np.array(np_data[2:, 2], dtype='float32')
 temp = np.array(np_data[2:, 3], dtype='float32')
 time = np.array(np_data[2:, 4], dtype='float32')
 ratings = np.array(np_data[2:, 5:9], dtype='float32')
+last_params = [weight[-2], temp[-2], time[-2]]
 current_params = [weight[-1], temp[-1], time[-1]]
 
 # calc rating stats
+np.seterr(invalid='ignore')  # nanstd returns 'nan' for days with only 1 review
 mean_ratings = scipy.stats.nanmean(ratings, axis=1)
 stdev_ratings = scipy.stats.nanstd(ratings, axis=1)
 
+last_mean = mean_ratings[-2]
+last_stdev = stdev_ratings[-2]
 current_mean = mean_ratings[-1]
 current_stdev = stdev_ratings[-1]
-last_mean = mean_ratings[-2]
+
+print u"          Weight (g) Temp (\N{DEGREE SIGN}F) Time (s) | Rating"
+print "--------------------------------------------------------------"
+print u"Last: {0:10.0f} {1:9.0f} {2:8.0f}     | {3:4.3f}\u00B1{4:4.3f}".format(
+        last_params[0],
+        last_params[1],
+        last_params[2],
+        last_mean,
+        last_stdev)
+print u"Current: {0:7.0f} {1:9.0f} {2:8.0f}     | {3:4.3f}\u00B1{4:4.3f}".format(
+        current_params[0],
+        current_params[1],
+        current_params[2],
+        current_mean,
+        current_stdev)
 
 # accept/reject
 if current_mean > last_mean:
-    print "Ratings improved since last step.\n"
+    print "\nRatings improved since last step.\n"
     new_vals = take_step(current_params)
 else:
     print "Ratings declined since last step.\n"
